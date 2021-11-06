1d0bdb1ba2f3369066c8b1e43b7c18423e233f3f
complesmat <complesmat@gmail.com>
2021-11-06 11:48:18 +1100

    Ajout du comp Word

--- decoy6/src/App.js
+++ decoy6/src/App.js
@@ -1,5 +1,12 @@
+import Word from './components/Word';
+
 function App() {
-  return <div>Decoy 6</div>;
+  return (
+    <div>
+      Decoy 6
+      <Word mot="Portemanteau" />
+    </div>
+  );
 }
 
 export default App;
